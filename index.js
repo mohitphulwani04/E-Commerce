efb5f0bb3cbe957f2ca77a1d87aebb0b71db5315
Mohit <phulwanimohit04@gmail.com>
2023-01-04 13:37:26 +0530

    Backend Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+
+dotenv.config()
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {console.log("DBConnnection Successful!")})
+.catch((err) => {
+    console.log(err)
+});
+
+
+app.use(express.json());
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!");
+})
+
+
